b842a9779c19ea7b30a8a765ebec9947b475563e
Anders Pitman <tapitman11@gmail.com>
2019-01-02 17:06:32 -0700

    Add Cancel

--- interfaces.go
+++ interfaces.go
@@ -1,6 +1,11 @@
 package omnicore
 
+type Streamer interface {
+        Cancel()
+}
+
 type Producer interface {
+        Streamer
         OnData(callback func([]byte))
         OnEnd(callback func())
         Request(numElements uint8)
